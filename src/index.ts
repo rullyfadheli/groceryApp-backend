61573b266fd8fdb81ba05e3dfebd23642b753517
rullyfadheli <rullyfadheli@gmail.com>
2025-07-03 22:43:20 +0700

    Refactor index.ts: remove unused response import, update allowedHeaders to array format, and organize route imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Request, response, Response } from "express";
+import { Request, Response } from "express";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -6,19 +6,25 @@ import cors from "cors";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import checkoutRouter from "./routes/checkoutRoutes.js";
+import couponRouter from "./routes/couponRoutes.js";
 const app = express();
+
+const onlineUsers = new Map<string, any>();
 
 const port = 3001;
 app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
-    allowedHeaders: "authorization",
+    allowedHeaders: ["authorization", "Content-Type"],
   })
 );
 app.use(cookieParser());
 app.use(express.json());
 
+app.use("/api", couponRouter);
+app.use("/api", checkoutRouter);
 app.use("/api", productRouter);
 app.use("/api", userRouter);
 app.use("/api", orderRoutes);
